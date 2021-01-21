7284e11d7b6fe058fe0e579dc3f3f66a2fc6d8fb
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-01-22 01:12:39 +0530

    Update Arrays_Python.py

--- Arrays/Arrays_Python.py
+++ Arrays/Arrays_Python.py
@@ -1,1 +1,36 @@
-
+# Python code to demonstrate the working of 
+# array(), append(), insert()
+  
+# importing "array" for array operations
+import array
+  
+# initializing array with array values
+# initializes array with signed integers
+arr = array.array('i', [1, 2, 3]) 
+ 
+# printing original array
+print ("The new created array is : ",end=" ")
+for i in range (0, 3):
+    print (arr[i], end=" ")
+ 
+print("\r")
+ 
+# using append() to insert new value at end
+arr.append(4);
+ 
+# printing appended array
+print("The appended array is : ", end="")
+for i in range (0, 4):
+    print (arr[i], end=" ")
+     
+# using insert() to insert value at specific position
+# inserts 5 at 2nd position
+arr.insert(2, 5)
+ 
+print("\r")
+ 
+# printing array after insertion
+print ("The array after insertion is : ", end="")
+for i in range (0, 5):
+    print (arr[i], end=" ")
+    
