8ce73bd47f7228210eee609dedefa17039f5969f
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-01-22 01:19:50 +0530

    Update Arrays_Python.py

--- Arrays/Arrays_Python.py
+++ Arrays/Arrays_Python.py
@@ -84,3 +84,34 @@ The new created array is : 1 2 3 1 5
 The popped element is : 3
 The array after popping is : 1 2 1 5 
 The array after removing is : 2 1 5 
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------  
+  
+# Python code to demonstrate the working of 
+# index() and reverse()
+  
+# importing "array" for array operations
+import array
+  
+# initializing array with array values
+# initializes array with signed integers
+arr= array.array('i',[1, 2, 3, 1, 2, 5]) 
+ 
+# printing original array
+print ("The new created array is : ",end="")
+for i in range (0,6):
+    print (arr[i],end=" ")
+ 
+print ("\r")
+ 
+# using index() to print index of 1st occurrenece of 2
+print ("The index of 1st occurrence of 2 is : ",end="")
+print (arr.index(2))
+ 
+#using reverse() to reverse the array
+arr.reverse()
+ 
+# printing array after reversing
+print ("The array after reversing is : ",end="")
+for i in range (0,6):
+    print (arr[i],end=" ")  
