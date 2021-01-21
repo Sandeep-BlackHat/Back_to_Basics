fe644c4c0c94f6c0225359a0d153cf73c8fb7c8d
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-01-22 01:16:09 +0530

    Update Arrays_Python.py

--- Arrays/Arrays_Python.py
+++ Arrays/Arrays_Python.py
@@ -39,3 +39,40 @@ Output:
 The new created array is : 1 2 3 
 The appended array is : 1 2 3 4 
 The array after insertion is : 1 2 5 3 4 
+
+  
+# Python code to demonstrate the working of 
+# pop() and remove() 
+ 
+# importing "array" for array operations 
+import array 
+ 
+# initializing array with array values 
+# initializes array with signed integers 
+arr= array.array('i',[1, 2, 3, 1, 5]) 
+ 
+# printing original array 
+print ("The new created array is : ",end="") 
+for i in range (0,5): 
+    print (arr[i],end=" ") 
+ 
+print ("\r") 
+ 
+# using pop() to remove element at 2nd position 
+print ("The popped element is : ",end="") 
+print (arr.pop(2)); 
+ 
+# printing array after popping 
+print ("The array after popping is : ",end="") 
+for i in range (0,4): 
+    print (arr[i],end=" ") 
+ 
+print("\r") 
+ 
+# using remove() to remove 1st occurrence of 1 
+arr.remove(1) 
+ 
+# printing array after removing 
+print ("The array after removing is : ",end="") 
+for i in range (0,3): 
+    print (arr[i],end=" ")  
