0a17dc479f3ba8951a28e4b0ec8346fa78914dc0
Sahil Verma <55850884+Keima483@users.noreply.github.com>
2021-01-26 16:48:28 +0530

    Update union_and_intersection_of_two_sorted_array.cpp

--- Arrays/Sahil/cpp/union_and_intersection_of_two_sorted_array.cpp
+++ Arrays/Sahil/cpp/union_and_intersection_of_two_sorted_array.cpp
@@ -1,3 +1,13 @@
+/*
+ * Given two sorted arrays, find their union and intersection.
+    Example:
+
+    Input : arr1[] = {1, 3, 4, 5, 7}
+            arr2[] = {2, 3, 5, 6} 
+    Output : Union : {1, 2, 3, 4, 5, 6, 7} 
+            Intersection : {3, 5}
+*/
+
 #include <iostream>
 #include <bits/stdc++.h>
 #include <array>
