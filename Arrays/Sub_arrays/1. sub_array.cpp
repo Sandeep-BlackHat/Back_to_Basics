fd2b3692ea210c8b380df6d5035f2d29e00b77a0
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-01-29 11:44:57 +0530

    Update 1. sub_array.cpp

--- Arrays/Sub_arrays/1. sub_array.cpp
+++ Arrays/Sub_arrays/1. sub_array.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void subArray(int arr[], int n)
+{
+    for (int i=0; i <n; i++)
+    {
+        for (int j=i; j<n; j++){
+		cout << arr[j] << " ";
+		}
+		cout<<"\n"<<endl;
+    }
+}
+ 
+int main()
+{
+    int arr[] = {1, 2, 3, 4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << "All Non-empty Subarrays\n";
+    subArray(arr, n);
+    return 0;
+}
