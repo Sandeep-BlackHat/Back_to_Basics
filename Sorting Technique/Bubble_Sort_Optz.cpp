2d66554c1beca1537f63d96b61277d6c75765da5
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-02-09 01:24:13 +0530

    Add files via upload

---
+++ Sorting Technique/Bubble_Sort_Optz.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+	int i, j, min_indx;
+	bool swapped;
+	for(i=0; i<n-1; i++)
+	{
+		for(j=i+1; j<n; j++)
+		{
+			if(arr[j] < arr[min_indx])
+			{
+				swap(&arr[min_indx], &arr[i]);
+				swapped = true; //Optimizing the previous code of Bubble sort adding this will save out time as if swap happend then
+								//only the loop will move else not.
+			}
+		}
+	}
+	if (swapped == false){
+		break;
+	}
+}
+
+void printArray(int arr[], int size)
+{
+	int i;
+	for(i=0; i<size; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+}
+
+int main()
+{
+	int i, n;
+	cout<<"Enter Size: ";
+	cin>>n;
+	int arr[n];
+	cout<<"\nEnter Array: ";
+	for(i=0; i<n; i++)
+	{
+		cin>>arr[i];
+	}
+	selectionSort(arr, n);
+	cout<<"\nArray after Sorting: ";
+	printArray(arr, n);
+	return 0;
+}
+
+/* Time Complexity: O(n*n) <Worst Case>
+				 O(n) <Best Case when array is sorted>
+Space Complexity: O(1) */
