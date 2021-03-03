a2469a9ef957ea04d34b51e80dbcda5de4101e58
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-03-03 10:17:25 +0530

    Update Kadane_Method2.py

--- Arrays/Sandeep/Python/Competitive Programming/Kadane_Method2.py
+++ Arrays/Sandeep/Python/Competitive Programming/Kadane_Method2.py
@@ -7,11 +7,11 @@ def maxSubArraySum(a, size):
 
     for i in range(0, size):
         max_ending_here = max_ending_here + a[i]
-        if(max_so_far < max_ending_here):
-            max_so_far = max_ending_here
+        if(max_so_far < 0):
+            max_so_far = 0
 
-        if(max_ending_here < 0):
-            max_ending_here = 0
+        elif(max_so_far < max_ending_here):
+            max_so_far = max_ending_here
     return max_so_far
 
 #Main_Code
