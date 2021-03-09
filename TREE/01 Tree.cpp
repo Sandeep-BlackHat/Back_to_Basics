8242e8236fa64bd038b4a739d7044785557b6142
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-03-09 23:01:49 +0530

    Add files via upload

---
+++ TREE/01 Tree.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+	int data;
+	struct Node* left;
+	struct Node* right;
+	
+	Node(int val)
+	{
+		data = val;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+int main()
+{
+	struct Node* root = new Node(0);
+	root->left = new Node(1);
+	root->right = new Node(2);
+	root->right->left = new Node(3);
+	root->left->left = new Node(4);
+	
+	return 0;
+}
+
