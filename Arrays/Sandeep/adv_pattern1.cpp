350eddf31301ce6f34350a21b637b19b89854992
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-05-05 21:30:30 +0530

    Add files via upload

---
+++ Arrays/Sandeep/adv_pattern1.cpp
@@ -0,0 +1,16 @@
+//column number pyramid pattern
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n; 
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
---
+++ Arrays/Sandeep/adv_pattern2.cpp
@@ -0,0 +1,16 @@
+//column number inverted pyramid pattern
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n; 
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n+1-i; j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
